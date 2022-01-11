8f03205ce73cee3469960ed27ecbbcbe1e9f1caa
harsimran-preet <59977147+harsimran-preet@users.noreply.github.com>
2022-01-10 17:37:29 -0800

    delete button added

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react';
 import Table from './Table'
 
 
@@ -23,11 +23,24 @@ const characters = [
 
 
 function MyApp() {
-  return (
-    <div className="container">
-      <Table characterData={characters} />
-    </div>
-  );  
+  const [characters, setCharacters] = useState([
+    {
+      name: 'Charlie',
+      job: 'Janitor',
+       // the rest of the data
+    },
+  ]);  
+  function removeOneCharacter (index) {
+    const updated = characters.filter((character, i) => {
+        return i !== index
+      });
+      setCharacters(updated);
+    }
+    return (
+      <div className="container">
+        <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      </div>
+    );  
 }
 
 export default MyApp;
--- src/Table.js
+++ src/Table.js
@@ -16,6 +16,9 @@ function TableBody(props) {
       <tr key={index}>
         <td>{row.name}</td>         
         <td>{row.job}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(index)}>Delete</button>
+      </td>
       </tr>
     );
    }
@@ -30,8 +33,9 @@ function TableBody(props) {
 function Table(props) {
   return (
     <table>
+      
       <TableHeader />
-      <TableBody characterData={props.characterData} />
+      <TableBody characterData={props.characterData} removeCharacter={props.removeCharacter} />
     </table>
   );
 }
